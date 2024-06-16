71fc7163de06c56f9859f354a0c4e29c1e2168b4
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2024-06-16 18:21:25 +0530

    Time: 1 ms (98.88%), Space: 41.4 MB (55.68%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false; // No corresponding open bracket for current close bracket
+                }
+                char openBracket = stack.pop();
+                if ((c == ')' && openBracket != '(') ||
+                    (c == ']' && openBracket != '[') ||
+                    (c == '}' && openBracket != '{')) {
+                    return false; // Mismatched close and open brackets
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
