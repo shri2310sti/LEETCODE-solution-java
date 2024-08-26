54a29780e99cb432cecf8e705e94cacf43c1502d
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2024-08-26 18:41:01 +0530

    Time: 1 ms (34.54%), Space: 44.7 MB (50.84%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        postorderHelper(root, result);
+        return result;
+    }
+    
+    private void postorderHelper(Node node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        for (Node child : node.children) {
+            postorderHelper(child, result);
+        }
+        result.add(node.val);
+    }
+}
