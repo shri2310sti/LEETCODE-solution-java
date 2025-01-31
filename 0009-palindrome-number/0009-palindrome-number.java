0e71e6dc17761ed97f7c86b4adb31cb305c73dfe
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-01-31 19:03:18 +0530

    Time: 187 ms (5.91%), Space: 45.2 MB (5.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,34 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int a = 0;
-        int b = x;
-
-        while(x>0){
-            int c = x%10;
-            a = a*10 + c;
-            x = x/10;
+        if(x<0) return false;
+        int temp = x;
+        
+        int index = 0;
+        
+        while(temp>0){
+            int digit = temp % 10;
+            temp/=10;
+            index++;
         }
-
-        if(a == b){
-            return true;
+        
+        int arr[] = new int[index];
+        
+        int count = 0;
+        int temp2 = x;
+        while(temp2>0){
+            int digit = temp2 % 10;
+            arr[count] = digit;
+            temp2 /= 10;
+            count++;
         }
-
-        return false;
+        
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] != arr[arr.length -1 - i]) return false;
+        }
+        return true;
         
     }
 }
