39a0da6d9b7aca16a6cfddb54ab4a724d362f66d
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-03-18 18:40:17 +0530

    Time: 0 ms (100%), Space: 45.9 MB (16.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,23 +3,44 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        int[] row = new int[m];
-        int[] col = new int[n];
+        int col0 = 1; // To track if the first column needs to be zeroed
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n;j++){
-                if(matrix[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
+        // Step 1: Mark rows and columns using the first row and column
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // Mark the row
+                    
+                    if (j == 0) {
+                        col0 = 0; // Mark first column
+                    } else {
+                        matrix[0][j] = 0; // Mark the column
+                    }
                 }
             }
         }
 
-        for(int i=0; i<m;i++){
-            for(int j=0; j<n;j++){
-                if(row[i] == 1 || col[j] == 1) matrix[i][j] = 0;
+        // Step 2: Set matrix elements to zero using marks from (1,1) to (m-1, n-1)
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
             }
         }
 
+        // Step 3: Zero out the first row if needed
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 4: Zero out the first column if needed
+        if (col0 == 0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
