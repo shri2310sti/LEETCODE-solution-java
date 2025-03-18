9db6556b0feb8b049fcdefe818228203720ccff3
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-03-18 18:49:47 +0530

    Time: 0 ms (100%), Space: 42.3 MB (40.35%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,30 +1,15 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        
-        // Iterate through each layer from outermost to innermost
-        for (int layer = 0; layer < n / 2; layer++) {
-            int first = layer;
-            int last = n - 1 - layer;
-            
-            // Iterate through each element in the current layer
-            for (int i = first; i < last; i++) {
-                int offset = i - first;
-                
-                // Save top
-                int top = matrix[first][i];
-                
-                // Left -> Top
-                matrix[first][i] = matrix[last - offset][first];
-                
-                // Bottom -> Left
-                matrix[last - offset][first] = matrix[last][last - offset];
-                
-                // Right -> Bottom
-                matrix[last][last - offset] = matrix[i][last];
-                
-                // Top -> Right
-                matrix[i][last] = top;
+        int[][] rotated = new int[n][n];
+        for(int i = 0; i<n;i++){
+            for(int j=0; j<n;j++){
+                rotated[j][n-i-1] = matrix[i][j];
+            }
+        }
+        for(int i = 0; i<n;i++){
+            for(int j=0; j<n;j++){
+                matrix[i][j] = rotated[i][j];
             }
         }
     }
