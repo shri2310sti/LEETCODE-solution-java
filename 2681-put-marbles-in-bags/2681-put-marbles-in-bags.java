9ae71528f5a7cf7ecf141a41e36692419a642f0c
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-03-31 17:53:29 +0530

    Time: 33 ms (84.33%), Space: 62.7 MB (65.67%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
@@ -1,10 +1,6 @@
 class Solution {
   public long putMarbles(int[] weights, int k) {
-    // To distribute marbles into k bags, there will be k - 1 cuts. If there's a
-    // cut after weights[i], then weights[i] and weights[i + 1] will be added to
-    // the cost. Also, no matter how we cut, weights[0] and weights[n - 1] will
-    // be counted. So, the goal is to find the max/min k - 1 weights[i] +
-    // weights[i + 1].
+    
     int[] arr = new int[weights.length - 1]; // weights[i] + weights[i + 1]
     long mn = 0;
     long mx = 0;
