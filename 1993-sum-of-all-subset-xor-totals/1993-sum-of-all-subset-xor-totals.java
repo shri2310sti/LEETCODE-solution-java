b11271ea53f42845e6d9325d1cf45729daea7096
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-04-05 22:58:54 +0530

    Time: 0 ms (100%), Space: 41.2 MB (36.98%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        return helper(nums, 0, 0);
+    }
+
+    private int helper(int[] nums, int index, int currentXor) {
+        if (index == nums.length) {
+            return currentXor;
+        }
+        
+        int with = helper(nums, index + 1, currentXor ^ nums[index]);
+        
+        int without = helper(nums, index + 1, currentXor);
+        
+        return with + without;
+    }
+}
