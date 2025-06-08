841ec8c75ad1e9a8cb0c9e6dd7c38dc28844176b
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-06-08 12:39:42 +0530

    Time: 2 ms (76.74%), Space: 48.3 MB (69.78%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 1; i < 10; i++) {
+            dfs(i, n, result);
+        }
+        
+        return result;
+    }
+    
+    private void dfs(int current, int n, List<Integer> result) {
+        if (current > n) {
+            return;
+        }
+        
+        result.add(current);
+        
+        for (int i = 0; i < 10; i++) {
+            int next = current * 10 + i;
+            if (next > n) {
+                break;
+            }
+            dfs(next, n, result);
+        }
+    }
+}
