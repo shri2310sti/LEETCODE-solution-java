a5d104fef2a30c4987474107651788412a5252e2
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-06-16 14:58:46 +0530

    Time: 0 ms (100%), Space: 42.1 MB (68.1%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -10,14 +10,14 @@ class Solution {
             
             if(nums[low] <= nums[mid]){
 
-                if(nums[low] <= target && target < nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
                     high = mid-1;
                 } else{
                     low = mid + 1;
                 }
             }
             else {
-                if(nums[mid] < target && target <= nums[high]){
+                if(nums[mid] <= target && target <= nums[high]){
                     low = mid + 1;
                 }
                 else high = mid - 1;
