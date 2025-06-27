64fc12c5f391a5f904e0260f2d477eec94cf4d49
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-06-27 18:11:04 +0530

    Time: 11 ms (75.74%), Space: 47.2 MB (59.18%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0; i < weights.length; i++){
+            if(max < weights[i]) max = weights[i];
+            sum += weights[i];
+        }
+
+        // Linear Search
+        // for(int i = max; i <= sum; i++){
+        //     int reqDays= days(weights, i);
+        //     if(reqDays <= days) return i;
+        // }
+
+        //Bianry Search
+        int low = max, high = sum;
+        
+        while(low <= high){
+            int mid = (low + high) / 2;
+            int noOfDays = days(weights, mid);
+            if(noOfDays > days){
+                low = mid + 1;
+            }
+            else {
+                
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+    static int days(int[] weights, int capacity){
+        int day = 1;
+        int load = 0;
+
+        for(int i = 0; i < weights.length; i++){
+            if((load + weights[i]) > capacity){
+                day++;
+                load = weights[i];
+            } else load += weights[i];
+
+        }
+        return day;
+    }
+
+}
