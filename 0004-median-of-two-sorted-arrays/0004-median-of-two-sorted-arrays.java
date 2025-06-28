78c11be2fdee59dfa1795b04f655096105695a1a
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-06-28 23:44:51 +0530

    Time: 2 ms (54.76%), Space: 46.2 MB (44.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,40 +1,45 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Ensure nums1 is the smaller array
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+        int n1 = nums1.length, n2 = nums2.length;
+        int n = n1 + n2;
+        int i = 0, j = 0;
 
-        int m = nums1.length;
-        int n = nums2.length;
-        int low = 0;
-        int high = m;
+        int ind2 = n/2;
+        int ind1 = ind2 - 1;
+        int cnt = 0;
+        int ind1el = -1, ind2el = -1;
 
-        while (low <= high) {
-            int i = (low + high) / 2;
-            int j = (m + n + 1) / 2 - i;
+        while(i < n1 && j < n2){
+            if(nums1[i] < nums2[j]){
+                if(cnt == ind1) ind1el = nums1[i];
+                if(cnt == ind2) ind2el = nums1[i];
 
-            // Calculate the partitions
-            int leftPart1 = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
-            int rightPart1 = (i == m) ? Integer.MAX_VALUE : nums1[i];
-            int leftPart2 = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
-            int rightPart2 = (j == n) ? Integer.MAX_VALUE : nums2[j];
-
-            // Check the partitioning conditions
-            if (leftPart1 <= rightPart2 && leftPart2 <= rightPart1) {
-                if ((m + n) % 2 == 0) {
-                    return (Math.max(leftPart1, leftPart2) + Math.min(rightPart1, rightPart2)) / 2.0;
-                } else {
-                    return Math.max(leftPart1, leftPart2);
-                }
-            } else if (leftPart1 > rightPart2) {
-                high = i - 1;
+                cnt++;
+                i++;
             } else {
-                low = i + 1;
+                if(cnt == ind1) ind1el = nums2[j];
+                if(cnt == ind2) ind2el = nums2[j];
+
+                cnt++;
+                j++;
             }
         }
+        while(i < n1){
+            if(cnt == ind1) ind1el = nums1[i];
+            if(cnt == ind2) ind2el = nums1[i];
 
-        // This should never be reached, but to make the compiler happy
-        throw new IllegalArgumentException("Input arrays are not sorted.");
+            cnt++;
+            i++;
+        }
+        while(j < n2){
+            if(cnt == ind1) ind1el = nums2[j];
+            if(cnt == ind2) ind2el = nums2[j];
+
+            cnt++;
+            j++;
+        }
+
+        if(n%2 == 1) return (double)ind2el;
+        return (double)((double)(ind1el + ind2el)) / 2.0;
     }
 }
