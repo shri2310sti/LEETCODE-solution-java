53e34c65964137597209fa9091224fab3efbe941
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-06-29 20:20:22 +0530

    Time: 2 ms (54.78%), Space: 45.8 MB (97.16%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,45 +1,31 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
         int n = n1 + n2;
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
         int i = 0, j = 0;
-
-        int ind2 = n/2;
-        int ind1 = ind2 - 1;
-        int cnt = 0;
-        int ind1el = -1, ind2el = -1;
-
         while(i < n1 && j < n2){
             if(nums1[i] < nums2[j]){
-                if(cnt == ind1) ind1el = nums1[i];
-                if(cnt == ind2) ind2el = nums1[i];
-
-                cnt++;
+                arr.add(nums1[i]);
                 i++;
             } else {
-                if(cnt == ind1) ind1el = nums2[j];
-                if(cnt == ind2) ind2el = nums2[j];
-
-                cnt++;
+                arr.add(nums2[j]);
                 j++;
             }
         }
         while(i < n1){
-            if(cnt == ind1) ind1el = nums1[i];
-            if(cnt == ind2) ind2el = nums1[i];
-
-            cnt++;
+            arr.add(nums1[i]);
             i++;
         }
         while(j < n2){
-            if(cnt == ind1) ind1el = nums2[j];
-            if(cnt == ind2) ind2el = nums2[j];
-
-            cnt++;
+            arr.add(nums2[j]);
             j++;
         }
 
-        if(n%2 == 1) return (double)ind2el;
-        return (double)((double)(ind1el + ind2el)) / 2.0;
+        if(n % 2 == 1) return arr.get(n/2);
+        return (double)((double)(arr.get(n/2) + arr.get(n/2 - 1)))/2.0;
     }
 }
