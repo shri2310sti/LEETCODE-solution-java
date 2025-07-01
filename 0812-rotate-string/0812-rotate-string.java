e7e539cdf53cdf20ed8ef4b3adaab614d562d866
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-07-01 18:23:05 +0530

    Time: 0 ms (100%), Space: 41.6 MB (52.16%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+        // Any rotation of s will be a substring of s + s.
+
         if(s.length() != goal.length()) return false;
-        return (s + s).contains(goal);
+       
+        return (s+s).contains(goal);
     }
 }
