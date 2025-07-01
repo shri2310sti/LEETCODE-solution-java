1b8f70ac5ce9239fef99594c225616388e1191d3
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-07-01 18:40:59 +0530

    Time: 4 ms (77.65%), Space: 43.2 MB (76.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -7,13 +7,11 @@ class Solution {
         int[] freq = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            freq[c - 'a']++; 
+            freq[s.charAt(i) - 'a']++; 
         }
         for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            freq[c - 'a']--; 
-            if (freq[c - 'a'] < 0) return false; 
+            freq[t.charAt(i) - 'a']--; 
+            if (freq[t.charAt(i) - 'a'] < 0) return false; 
         }
 
         return true;
