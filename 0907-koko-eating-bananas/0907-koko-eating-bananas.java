c226f0aa6729e112cb9dc9156edc1f10ffabdd05
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-07-03 17:59:59 +0530

    Time: 26 ms (26.45%), Space: 45.6 MB (9.39%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,38 +1,29 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-
         int maxPile = Integer.MIN_VALUE;
-        for(int i = 0; i < piles.length; i++){
+        for(int i = 0; i<piles.length; i++){
             if(maxPile < piles[i]) maxPile = piles[i];
         }
-
-        
         // for(int i = 1; i <= maxPile; i++){
-        //     int RequiredTime = totalTime(piles,i);
-        //     if(RequiredTime <= h) return i; 
+        //     if(func(piles, i) <= h) return i;
         // }
-
+        
         int low = 1, high = maxPile;
-        int ans = Integer.MAX_VALUE;
-
+        int ans = 0;
         while(low <= high){
-            int mid = (low + high) / 2;
-            int RequiredTime = totalTime(piles, mid);
-            if(RequiredTime <= h) {
-                ans = Math.min(ans, mid);
+            int mid = (low + high)/2;
+            if(func(piles, mid) <= h){
+                ans = mid;
                 high = mid - 1;
-            }
-            else low = mid + 1;
-
-
+            } else low = mid + 1;
         }
         return ans;
     }
-    public int totalTime(int[] arr, int hourly){
-        int totalhrs = 0;
-        for(int i = 0; i < arr.length; i++){
-            totalhrs += Math.ceil((double)(arr[i]) / (double)(hourly));
+    static int func(int[] piles, int bananaperhr){
+        int ans = 0;
+        for(int i = 0; i < piles.length; i++){
+            ans += Math.ceil((double)piles[i]/(double)bananaperhr);
         }
-        return totalhrs;
+        return ans;
     }
 }
