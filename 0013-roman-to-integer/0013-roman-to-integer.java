bcdad6a124900560186a9d92bc823cf1f2218c8d
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-07-10 13:23:14 +0530

    Time: 2 ms (100%), Space: 44.8 MB (51.2%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,41 +1,26 @@
 class Solution {
     public int romanToInt(String s) {
-        int num = 0;
-        int prevValue = 0;
+        int preV = 0;
+        int ans = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            int curr = getValue(s.charAt(i));
 
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int currValue = getValue(s.charAt(i));
-            
-            if (currValue < prevValue) {
-                num -= currValue;
-            } else {
-                num += currValue;
-            }
-
-            prevValue = currValue;
+            if(preV > curr) ans -= curr;
+            else ans += curr;
+            preV = curr;
         }
-
-        return num;
+        return ans ;
     }
-
-     private int getValue(char romanChar) {
-        switch (romanChar) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-            default:
-                return 0;
+    private int getValue (char ch){
+        switch(ch){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return 0;
         }
     }
 }
