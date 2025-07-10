f5c8600eae65f4ee1894f7e17f16aa1153693d9d
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-07-10 23:51:15 +0530

    Time: 1 ms (100%), Space: 42.2 MB (96.51%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -3,10 +3,7 @@ class Solution {
         s = s.trim();
         if(s.isEmpty()) return 0;
 
-
-        int i = 0; 
-        int ans = 0;
-        int sign = 1;
+        int i = 0, ans = 0, sign = 1;
         if(s.charAt(i) == '-') {
             sign = -1;
             i++;
@@ -17,9 +14,7 @@ class Solution {
         }
         while(i < s.length() && Character.isDigit(s.charAt(i))){
             int digit = s.charAt(i) - '0';
-            // if(ans > Integer.MAX_VALUE / 10) return (sign == -1) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-            if (ans > Integer.MAX_VALUE / 10 || 
-                ans == Integer.MAX_VALUE / 10 && digit > 7) {
+            if (ans > Integer.MAX_VALUE / 10 || ans == Integer.MAX_VALUE / 10 && digit > 7) {
                 return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
             ans = ans * 10 + digit;
