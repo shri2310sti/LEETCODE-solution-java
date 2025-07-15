6060b520d3f8512dfc638f7375b66c7795d7bfa5
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-07-15 18:24:36 +0530

    Time: 0 ms (100%), Space: 43 MB (35.7%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -1,28 +1,41 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int i = 0;
-        int j = 1;
+        // int i = 0;
+        // int j = 1;
+
+        // int n = arr.length;
+        // int last = arr[n-1] + k;
+
+        // int count = 0;
+
+        // while(i < n && j <= last){
+        //     if(arr[i] == j){
+        //         i++;
+        //         j++;
+        //     } else {
+        //         count++;
+        //         if(count == k) return j;
+        //         j++;
+        //     }
+        // }
+        // while(j <= last){
+        //     count++;
+        //     if(count == k) return j;
+        //     j++;
+        // }
+        // return -1;
 
         int n = arr.length;
-        int last = arr[n-1] + k;
-
-        int count = 0;
-
-        while(i < n && j <= last){
-            if(arr[i] == j){
-                i++;
-                j++;
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            int missing = arr[mid] - (mid + 1);
+            if(missing < k){
+                low = mid + 1;
             } else {
-                count++;
-                if(count == k) return j;
-                j++;
+                high = mid - 1;
             }
         }
-        while(j <= last){
-            count++;
-            if(count == k) return j;
-            j++;
-        }
-        return -1;
+        return k + high + 1;
     }
 }
