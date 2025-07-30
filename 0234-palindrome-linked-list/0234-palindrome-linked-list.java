174337c76313daca23cc5fc20e05b3951f7ee985
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-07-30 12:23:37 +0530

    Time: 3 ms (99.71%), Space: 59.2 MB (72.53%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -33,12 +33,5 @@ class Solution {
        }
        return true;
     }
-    // private static ListNode reverse(ListNode head){
-    //     if(head == null || head.next == null) return head;
-    //     ListNode newHead = reverse(head.next);
-    //     ListNode front = head.next;
-    //     front.next = head;
-    //     head.next = null;
-    //     return newHead;
-    // }
+    
 }
