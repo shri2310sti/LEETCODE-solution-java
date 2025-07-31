6bdcba9e5ae36f0f1baf5d2e7180bc9d47bd85e1
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-07-31 10:37:35 +0530

    Time: 0 ms (100%), Space: 44.4 MB (90.65%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,22 +10,18 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
+        if(head == null || head.next == null || head.next.next == null) return head;
 
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode evenHead = even; // To keep the head of even list to attach at the end of odd list
-
-        while (even != null && even.next != null) {
+        ListNode even = head.next, odd = head;
+        
+        ListNode oddFirst = head.next;
+        while(even != null && even.next != null){
             odd.next = even.next;
             odd = odd.next;
             even.next = odd.next;
             even = even.next;
         }
-
-        odd.next = evenHead; // Attach the even list at the end of odd list
+        odd.next = oddFirst;
         return head;
     }
 }
