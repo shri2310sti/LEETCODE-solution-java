9cb4aab81cd2740cf55d7b0a7391730283566e33
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-08-01 11:53:41 +0530

    Time: 1 ms (82.66%), Space: 41.9 MB (91%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -2,37 +2,21 @@ class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ans = new ArrayList<>();
 
-        // for(int i = 1; i <= numRows; i++){
-        //     List<Integer> ansRow = new ArrayList<>();
-        //     for(int j = 1; j <= i; j++){
-        //         ansRow.add(nCr(i-1,j-1));
-        //     }
-        //     ans.add(ansRow);
-        // }
         for(int i = 1; i <= numRows; i++){
-            ans.add(generateRow(i));
+            List<Integer> ansRow = new ArrayList<>();
+            for(int j = 1; j <= i; j++){
+                ansRow.add(nCr(i-1,j-1));
+            }
+            ans.add(ansRow);
         }
         return ans;
     }
-
-    public List<Integer> generateRow(int n){
-        List<Integer> result = new ArrayList<>();
-        int ans = 1;
-        result.add(ans);
-        for(int i = 1; i < n; i++){
-            ans = ans * (n-i);
-            ans = ans / i;
-            result.add(ans);
+    public int nCr(int n, int r){
+        int result = 1; // 4C2
+        for(int i = 0; i < r; i++){
+            result = result * (n-i); // 1 4 3 
+            result = result / (i+1); 
         }
         return result;
-
     }
-    // public int nCr(int n, int r){
-    //     int result = 1; // 4C2 -> element(5,3)
-    //     for(int i = 0; i < r; i++){
-    //         result = result * (n-i); // 4 3 
-    //         result = result / (i+1); // 1 2
-    //     }
-    //     return result;
-    // }
 }
