b177c0fad5f3e46bc42210b9f57c0fec84e18264
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-08-11 15:54:51 +0530

    Time: 0 ms (100%), Space: 43.3 MB (16%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,29 @@
-import java.lang.Math;
 class Solution {
+    public double pospow(double x,int n){
+        if(n<=1){
+            return x;
+        }
+        double a = pospow(x,n/2);
+        if(n%2==0){
+            return a*a;
+        }
+        return pospow(x,n/2+1)*a;
+    }
     public double myPow(double x, int n) {
-        double result = (Math.pow(x,n));
-        return result;
+        if(x==-1){
+            if(n%2==0) return 1;
+            return -1;
+        }
+        if(n==0||x==1) return 1;
+        if(n==Integer.MIN_VALUE){
+            x =0;
+            return x;
+        }
+        if(n>=0){
+            return pospow(x,n);
+        }
+        double r = 1/x;
+        n*=-1;
+        return pospow(r,n);
     }
 }
