a2e682b558daf177c691d2e8bfae81e9916f77ea
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-12-13 12:48:28 +0530

    Time: 3 ms (88.81%), Space: 45 MB (66.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,13 +5,13 @@ class Solution {
         int n = s.length();
         int l = 0, r = 0, maxLength = 0;
         while(r < n){
-           
+            if(hash[s.charAt(r)] != -1){
                 if(hash[s.charAt(r)] >= l) l = hash[s.charAt(r)] + 1;
-                int len = r-l+1;
-                maxLength = Math.max(len, maxLength);
-                hash[s.charAt(r)] = r;
-                r++;
-            
+            } 
+            int len = r-l+1;
+            maxLength = Math.max(len, maxLength);
+            hash[s.charAt(r)] = r;
+            r++;
         }
         return maxLength;
     }
