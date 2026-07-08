19b565d317435a5b8787a8adc298d0dfb48d0726
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2026-07-08 17:04:06 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
 
-        // Base cases
         dp[0] = 1;
         dp[1] = 1;
 
-        // Fill dp array using bottom-up dynamic programming
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for(int i=2; i<=n ;i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-
-        // Print the nth Fibonacci number
-        return(dp[n]);
+        return dp[n];
     }
 
 }
